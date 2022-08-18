23a7de80994ead5596c942b84e6e8edc372d1c8f
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-08-17 22:44:59 -0500

    Desarrollo del Challenge 2

--- Ejercicios/Sources/Ejercicios/main.swift
+++ Ejercicios/Sources/Ejercicios/main.swift
@@ -1,9 +1,13 @@
 var argumento: String = CommandLine.arguments[1]
 
-if (argumento == "1") {
+if (argumento == "0") {
     Challenge0().fizzBuzz()
-} else if (argumento == "2") {
+} else if (argumento == "1") {
     print(Challenge1().isAnagram(wordOne: "PRUEBA", wordTwo:"prueba"))
     print(Challenge1().isAnagram(wordOne: "Roma", wordTwo:"amor"))
     print(Challenge1().isAnagram(wordOne: "prueba", wordTwo:"12121"))
+} else if (argumento == "2") {
+    Challenge2().fibonacci()
+} else {
+    print("Sin implementación")
 }
